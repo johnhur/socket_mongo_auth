05d6518e45f763c63b32152734b7f40946ece645
johnhur <johnjhur@gmail.com>
2015-06-29 11:46:47 -0700

    heroku and mongoLab installed

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/socket_auth");
+mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/socket_auth");
 mongoose.set("debug", true);
 
 module.exports.User = require("./user")
